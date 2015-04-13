a6710d4dc3cebaf936a6b447193d55cf11a11563
paulwallis42 <paulwallis42@gmail.com>
2015-04-13 13:33:47 +0100

    passing first two tests

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -10,6 +10,28 @@ describe('Javabuzz', function() {
 
     });
 
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+
+    });
+
+  });
+
+  describe('knows when a number is NOT', function() {
+
+    it('divisible by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false)
+
+    });
+
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false)
+
+    });
+
   });
 
 });
